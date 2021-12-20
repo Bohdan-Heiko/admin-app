a2a3bb2ab99cc9c82145437d459c8159ebbd5410
Bohdan <bgejko@gmaol.com>
2021-12-20 17:36:55 +0200

    add product page

--- client/src/component/ProductPage/ProductCreate/index.js
+++ client/src/component/ProductPage/ProductCreate/index.js
@@ -2,17 +2,17 @@ import React from 'react'
 import {Create, SimpleForm, TextInput, DateInput} from 'react-admin'
 
 
-const PostCreate = (props) => {
+const ProductCreate = (props) => {
 
   return (
     <Create title='Create new post' {...props}>
       <SimpleForm>
-        <TextInput source='title' />
-        <TextInput multiline source='body' />
+        <TextInput source='productname' />
+        <TextInput multiline source='description' />
         <DateInput label='Published' source='publishedAt' />
       </SimpleForm>
     </Create>
   )
 }
 
-export default PostCreate;
+export default ProductCreate;
